dfcbb490d00ceddf7e3848a8a9c9570dd0eb8f2e
RetroRebirth <chrsw49@gmail.com>
2015-04-14 15:37:00 -0700

    world class, removed commented code (WIP)

--- main.cpp
+++ main.cpp
@@ -25,10 +25,6 @@ GLint h_uV;
 GLint h_uM;
 GLint h_uView;
 GLint h_uAClr, h_uDClr, h_uSClr, h_uS;
-/*
-// Ground
-bufID_t groundBufIDs;
-*/
 
 /** UTILITY **/
 float randF() {
@@ -183,56 +179,7 @@ void createObject() {
    objects.push_back(object);
 }
 
-/*
-void initGround() {
-   // Position array of ground
-   GLfloat vertices[] = {
-      -1.0f, 0.0, -1.0f, //0
-      -1.0f, 0.0, +1.0f, //1
-      +1.0f, 0.0, -1.0f, //2
-      +1.0f, 0.0, +1.0f, //3
-   };
-   // Amplify ground size
-   for (int i = 0, last = sizeof(vertices)/sizeof(GLfloat); i < last; i++) {
-      // Don't amplify the floor height
-      if (i % 3 != 1)
-         vertices[i] *= SIZE;
-   }
-   glGenBuffers(1, &(groundBufIDs.pos));
-   glBindBuffer(GL_ARRAY_BUFFER, groundBufIDs.pos);
-   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-   // Index array of ground
-   GLuint indices[] = {
-      0, 1, 2,
-      1, 3, 2,
-   };
-   glGenBuffers(1, &(groundBufIDs.ind));
-   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, groundBufIDs.ind);
-   glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-   // Normal array of ground
-   vector<float> posBuf(vertices, vertices+sizeof(vertices)/sizeof(vertices[0]));
-   vector<unsigned int> indBuf(indices, indices+sizeof(indices)/sizeof(indices[0]));
-
-   glGenBuffers(1, &(groundBufIDs.nor));
-   glBindBuffer(GL_ARRAY_BUFFER, groundBufIDs.nor);
-}
-*/
-
 void initGL() {
-/*
-   loadShapes(objectFiles[0]);
-
-   // Initialize GLEW
-   if (glewInit() != GLEW_OK) {
-      fprintf(stderr, "Failed to initialize GLEW\n");
-      exit(-1);
-   }
-
-   installShaders("vert.glsl", "frag.glsl");
-*/
-
    // Enable alpha drawing
    glEnable (GL_BLEND);
    glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
@@ -241,7 +188,6 @@ void initGL() {
    // Set the background color
    glClearColor(0.6f, 0.6f, 0.8f, 1.0f);
 
-//   initGround();
    world->initGround();
 
    // Unbind the arrays
@@ -251,29 +197,6 @@ void initGL() {
 }
 
 /** DRAWING **/
-/*
-void drawGround() {
-   // Bind position buffer
-   glBindBuffer(GL_ARRAY_BUFFER, groundBufIDs.pos);
-   glVertexAttribPointer(h_aPos, 3, GL_FLOAT, GL_FALSE, 0, 0);
-   // Bind index buffer
-   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, groundBufIDs.ind);
-   // Bind normal buffer
-   glVertexAttribPointer(h_aNor, 3, GL_FLOAT, GL_FALSE, 0, 0);
-   // Set color
-   glUniform3f(h_uAClr, 0.13, 0.13, 0.14);
-   glUniform3f(h_uDClr, 0.0, 0.0, 0.0);
-   glUniform3f(h_uSClr, 0.0, 0.0, 0.0);
-   glUniform1f(h_uS, 1.0);
-   // Apply translation
-   glm::vec3 trans = glm::vec3(0.0, 0.0, 0.0);
-   glm::mat4 T = glm::translate(glm::mat4(1.0f), trans);
-   safe_glUniformMatrix4fv(h_uM, glm::value_ptr(T));
-
-   glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-}
-*/
-
 void drawGL() {
    // Clear the screen
    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -287,7 +210,6 @@ void drawGL() {
    camera->setProjectionMatrix(window->width, window->height);
    camera->setView();
 
-//   drawGround();
    world->drawGround();
 /*
    for (vector<Object>::iterator it = objects.begin(); it != objects.end(); ++it) {
