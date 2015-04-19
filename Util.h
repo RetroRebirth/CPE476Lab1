9bdcd7d22e7200360d9625eb18ad76fd8d197313
IzumiMirai <alannabuss@gmail.com>
2015-04-19 15:40:51 -0700

    Added the degreesToRadians function and the radiansToDegrees function.

--- Util.h
+++ Util.h
@@ -4,17 +4,31 @@
 #include <cmath> 
 namespace Util
 {
-    
+    //Returns a random float 
     float randF()
     {
       return (float) rand() / RAND_MAX;
     }
-
+    
+    //Safely uses the glUniformMatrix4fv function
     void safe_glUniformMatrix4fv(const GLint handle, const GLfloat data[])
     {
       if(handle >= 0)
 	glUniformMatrix4fv(handle, 1, GL_FALSE, data);
     }
+    
+    //Changes Degrees to radians
+    float degreesToRadians(float degrees)
+    {
+      return degrees * 0.0174532925;
+    }
+
+    //changes radians to degrees.
+    float radiansToDegrees(float radians)
+    {
+      return radians * 57.2957795;
+    }
+
 }
 
 #endif
